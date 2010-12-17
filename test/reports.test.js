cfecc071b34c4bc36dd11909c93b9e15aca71868
Dylan Greene <dylang@gmail.com>
2010-12-17 16:50:29 -0500

    use callback, update package

--- test/reports.test.js
+++ test/reports.test.js
@@ -4,21 +4,15 @@ var log = require('logging').from(__filename);
 var Jobvite = require('../lib/jobvite'),
     Reports = Jobvite.Reports;
 
-function ready() {
-    log('all data has been read');
-}
-
-function onComplete(data) {
-    log(data);
+function ready(data) {
+   log(data);
 }
 
 
 Reports.config({
     report_id: false, //report id
     jobvite_username: false, //email address
-    jobvite_password: false, //use your password
-    onComplete: onComplete
-
+    jobvite_password: false //use your password
 });
 
 Reports.load(ready);
