a1b8e42f85e36db74bd7952b7f258df7d4a01edf
Dylan Greene <dylang@gmail.com>
2011-04-04 22:27:05 -0400

    huge update for better job listing support

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -4,29 +4,33 @@ var log = require('logging').from(__filename);
 var Jobvite = require('../lib/jobvite'),
     Jobs = Jobvite.Jobs;
 
-var jobData;
-
-function ready() {
-    module.exports['init'] = function(assert){
-        log('init');
-        jobData = Jobs.data();
-        assert.isDefined(jobData);
-   };
-
-
-    module.exports['download'] = function(assert){
-        log('download');
-        var saveDataBoolean = false;
-        function callback(changes){
-            assert.isDefined(changes);
-        }
-
-        jobData = Jobs.reload(callback, saveDataBoolean);
-
-    };
-}
 Jobs.config({
-    jobvite_company_id: 'qgY9Vfw2',
-    cache_directory: './test/data'
-}).init(ready);
+                jobvite_company_id: 'qgY9Vfw2',
+                cache_directory: './test/data'
+            });
+
+var testCase = require('nodeunit').testCase;
+
+module.exports = {
+        load: function (test) {
+            test.expect(2);
+
+            Jobs.load(function(err, data){
+                test.ok(!err);
+                test.ok(data);
+                test.done();
+            });
+        },
+        reload: function(test){
+            test.expect(2);
+            var saveDataBoolean = false;
+            Jobs.reload(cb, saveDataBoolean);
+            function cb(err, data){
+                test.ok(!err);
+                test.ok(data);
+                test.done();
+            }
+        }
+};
+
 
